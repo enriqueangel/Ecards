7f52df6959ecad6613f361a133a2fd1dd79f61dc
enriqeangel <enriqe.angel.596@gmail.com>
2017-12-14 19:07:48 -0500

    volver a subir

--- app/src/main/java/com/example/enriq/ecards/MainActivity.java
+++ app/src/main/java/com/example/enriq/ecards/MainActivity.java
@@ -1,175 +1,68 @@
 package com.example.enriq.ecards;
 
-import android.content.Intent;
-import android.graphics.Typeface;
-import android.support.design.widget.TextInputLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.util.Patterns;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-// Nuevos
-import android.text.method.ScrollingMovementMethod;
-import com.android.volley.RequestQueue;
-import com.android.volley.VolleyError;
-import com.android.volley.Response;
-import com.android.volley.Request;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextInputLayout campoCorreo;
-    private TextInputLayout campoContrasena;
-    private Typeface Roboto;
-
-    EditText correo, contrasena;
-    Button btn_singin, btn_singup;
-    RequestQueue requestQueue;
-
-    boolean email = false, password = false;
-    String baseUrl = "https://webserver-enriqeangel.c9users.io/";
-    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String fuente = "fuentes/Roboto.ttf";
-        this.Roboto = Typeface.createFromAsset(getAssets(), fuente);
+        // Opciones de ViewPager para cada Tabs
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
 
-        // Inicializo campos del layout
-        correo = (EditText) findViewById(R.id.correo);
-        contrasena = (EditText) findViewById(R.id.contrasena);
-        btn_singin = (Button) findViewById(R.id.btn_singin);
-        btn_singup = (Button) findViewById(R.id.btn_singup);
-        campoCorreo = (TextInputLayout) findViewById(R.id.campo_correo);
-        campoContrasena = (TextInputLayout) findViewById(R.id.campo_contrasena);
+        // Establecer Tabs dentro de Toolbar
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
 
-        requestQueue = Volley.newRequestQueue(this);
+    // Agregar fragmentos a Tabs
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new SignInFragment(), "Inicio Sesión");
+        adapter.addFragment(new SignUpFragment(), "Registro");
+        viewPager.setAdapter(adapter);
+    }
 
-        campoCorreo.setTypeface(Roboto);
-        campoContrasena.setTypeface(Roboto);
-        btn_singin.setTypeface(Roboto);
-        btn_singup.setTypeface(Roboto);
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
 
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
 
-        // Cuando se le da clik al boton genera evento
-        btn_singin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Obtiene valor del campo de correo y valida si es formato email
-                if(Patterns.EMAIL_ADDRESS.matcher(correo.getText().toString()).matches() == false) {
-                    email = false;
-                    campoCorreo.setError("Correo erroneo");
-                    // Toast.makeText(getApplicationContext(), "Correo erroneo", Toast.LENGTH_SHORT).show();
-                } else {
-                    email = true;
-                    campoCorreo.setError(null);
-                }
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
 
-                // Valida que la contraseña no este vacia
-                if(TextUtils.isEmpty(contrasena.getText())) {
-                    campoContrasena.setError("Ingrese contraseña");
-                    password = false;
-                } else {
-                    campoContrasena.setError(null);
-                    password = true;
-                    // Toast.makeText(getApplicationContext(), "Ingrese contraseña", Toast.LENGTH_SHORT).show();
-                }
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
 
-                if(email && password){
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
 
-                    Intent intent = new Intent(MainActivity.this, Crear_Pin.class);
-                    intent.putExtra( "Correo", correo.getText().toString()  );
-                    startActivity(intent);
-
-                    /*
-                    final String correo_inp = correo.getText().toString();
-                    final String password_inp = contrasena.getText().toString();
-
-                    // ---------- Metodo Post ----------
-                    url = baseUrl + "login";
-
-                    Map<String, String>  params = new HashMap<String, String>();
-                    params.put("correo", correo_inp);
-                    params.put("password", password_inp);
-
-                    JsonObjectRequest arrReq = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params),
-                            new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    try {
-                                        String respuesta = response.get("respuesta").toString();
-                                        if(respuesta.equals("si")){
-                                            Intent i = new Intent(getApplicationContext(), Card.class);
-                                            startActivity(i);}
-                                            //Toast.makeText(getApplicationContext(), "Usuario existe", Toast.LENGTH_SHORT).show();
-                                        else
-                                            Toast.makeText(getApplicationContext(), "Usuario no existe", Toast.LENGTH_SHORT).show();
-                                    } catch (JSONException e) {
-                                        Log.e("Volley", "Invalid JSON Object.");
-                                        Toast.makeText(getApplicationContext(), "Otro error", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            },
-                            new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    Log.e("Volley", error.toString());
-                                    Toast.makeText(getApplicationContext(), "Error en la conexion", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                    ); */
-
-                    // ---------- Metodo Get ----------
-                    // url = baseUrl + "login?correo=" + correo_inp + "&password=" + password_inp;
-
-                    /*JsonObjectRequest arrReq = new JsonObjectRequest(Request.Method.GET, url,
-                            new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    try {
-                                        String respuesta = response.get("respuesta").toString();
-                                        // Toast.makeText(getApplicationContext(), respuesta, Toast.LENGTH_SHORT).show();
-                                        if(respuesta.equals("si"))
-                                            Toast.makeText(getApplicationContext(), "Usuario existe", Toast.LENGTH_SHORT).show();
-                                        else
-                                            Toast.makeText(getApplicationContext(), "Usuario no existe", Toast.LENGTH_SHORT).show();
-                                    } catch (JSONException e) {
-                                        Log.e("Volley", "Invalid JSON Object.");
-                                    }
-                                }
-                            },
-                            new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    Log.e("Volley", error.toString());
-                                    Toast.makeText(getApplicationContext(), "Error en la conexion", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-
-                    );
-
-                    requestQueue.add(arrReq);
-                    */
-                }
-            }
-        });
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
