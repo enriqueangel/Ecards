6061aaf4e0997d401bf0343e9447c4fc9d3c4a75
ValentinaRojas <31673137+ValentinaRojas@users.noreply.github.com>
2018-01-06 14:30:56 -0500

    Vista de Perfil.

---
+++ app/src/main/java/com/example/enriq/ecards/editperfil.java
@@ -0,0 +1,13 @@
+package com.example.enriq.ecards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editperfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_editperfil);
+    }
+}
