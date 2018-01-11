598aecac393026df58cd2f15913afca649e2b66f
laurasgm <laurasgm@github>
2018-01-11 01:28:57 -0500

    Merge branch 'master' of C:\Users\Laura\AndroidStudioProjects\Ecards with conflicts.

---
+++ app/src/main/java/com/example/enriq/ecards/Crear_Tarjeta.java
@@ -0,0 +1,8 @@
+package com.example.enriq.ecards;
+
+/**
+ * Created by Laura on 9/01/2018.
+ */
+
+public class Crear_Tarjeta {
+}
---
+++ app/src/main/java/com/example/enriq/ecards/ViewAdapter.java
@@ -0,0 +1,8 @@
+package com.example.enriq.ecards;
+
+/**
+ * Created by Laura on 10/01/2018.
+ */
+
+public class ViewAdapter {
+}
