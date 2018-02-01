f1ce6e8f9993ddd13910930cac09f60a82406cf1
enriqeangel <enriqe.angel.596@gmail.com>
2018-02-01 18:19:43 -0500

    Arreglos

--- app/src/main/java/com/example/enriq/ecards/perfil.java
+++ app/src/main/java/com/example/enriq/ecards/perfil.java
@@ -1,8 +1,11 @@
 package com.example.enriq.ecards;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,15 +18,16 @@ public class perfil extends AppCompatActivity {
 
     JSONObject DATOS;
     TextView Correo,Nombres,Apellidos,Telefono,Rama,HrsLaborales;
-    Button EditarDAtos;
-
-
-
+    FloatingActionButton EditarDAtos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.acti_perfil);
+        setContentView(R.layout.activity_perfil);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Correo = (TextView) findViewById(R.id.TXTVCorreo);
         Nombres = (TextView) findViewById(R.id.TXTVNombres);
@@ -31,14 +35,13 @@ public class perfil extends AppCompatActivity {
         Telefono = (TextView) findViewById(R.id.TXTVTelefono);
         Rama = (TextView) findViewById(R.id.TXTVRama);
         HrsLaborales = (TextView) findViewById(R.id.TXTVHrsLaborales);
-        EditarDAtos = (Button) findViewById(R.id.BTNEditarDatos);
+        EditarDAtos = (FloatingActionButton) findViewById(R.id.BTNEditarDatos);
 
         try {
             DATOS = new JSONObject(getIntent().getStringExtra("DATOS"));
 
+            toolbar.setTitle(DATOS.get("nombres").toString() + " " + DATOS.get("apellidos").toString());
             Correo.setText(DATOS.get("email").toString());
-            Nombres.setText(DATOS.get("nombres").toString());
-            Apellidos.setText(DATOS.get("apellidos").toString());
             Telefono.setText(DATOS.get("telefono").toString());
             Rama.setText(GetRamas(DATOS.getJSONArray("areas")));
             HrsLaborales.setText(DATOS.get("horas_contratadas").toString());
@@ -56,22 +59,11 @@ public class perfil extends AppCompatActivity {
                 finish();
             }
         });
-
-
-
-
-
-
     }
 
-
-
     private String GetRamas(JSONArray jsonArray) throws JSONException {
-
         String Ramas = "";
-
         Boolean Primero = true;
-
 
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject Rama = jsonArray.getJSONObject(i);
@@ -80,15 +72,21 @@ public class perfil extends AppCompatActivity {
             };
 
             Ramas += Rama.getString("nombre");
-
             Primero = false;
-
-
         }
-
         return Ramas;
-
     };
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
 
