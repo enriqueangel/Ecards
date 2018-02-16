85605a2259bb85afa67da8ce38274af201a1e49e
enriqeangel <enriqe.angel.596@gmail.com>
2018-02-15 19:47:00 -0500

    Fragments crear tester

--- app/src/main/java/com/example/enriq/ecards/SpinnerUserFragment.java
+++ app/src/main/java/com/example/enriq/ecards/SpinnerUserFragment.java
@@ -8,13 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class TarjetasFragment extends Fragment {
+public class SpinnerUserFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_tarjetas, container, false);
+        View view = inflater.inflate(R.layout.fragment_spinner_user, container, false);
         return view;
     }
 }
