0bf27f9f1f30f488e88215d5e58dfa5b54a65be9
ronaldfelipse <ronald-1524@hotmail.com>
2018-03-28 14:10:30 -0500

    Configuracion firebase 2

---
+++ app/src/main/java/com/woldev/enriq/ecards/FireBaseInstanceIDService.java
@@ -0,0 +1,15 @@
+package com.woldev.enriq.ecards;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by jose on 28/03/2018.
+ */
+
+public class FireBaseInstanceIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+    }
+}
---
+++ app/src/main/java/com/woldev/enriq/ecards/FireBaseMessagingService.java
@@ -0,0 +1,16 @@
+package com.woldev.enriq.ecards;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by jose on 22/03/2018.
+ */
+
+public class FireBaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
